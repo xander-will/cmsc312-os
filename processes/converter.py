b23d7bd821bdf55f27f31e3ff8eaadb7947ce812
xander-will <xander@cavecreations.net>
2019-10-03 14:34:20 -0400

    converter is finished for the moment

--- processes/converter.py
+++ processes/converter.py
@@ -1,4 +1,21 @@
+## 
+##  CMSC 312
+##  Xander Will
+##
+##  'converter.py'
+##  "Creates a bytecode representation of a json process file"
+##
+##  October 2019
+##
+
+import glob
+import sys
 import json
+
+def walk_gen():
+    for name in glob.glob("./*.json"):
+        print(f"Found {name}")
+        yield name
 
 def file_gen(paths):
     for p in paths:
@@ -11,7 +28,8 @@ def file_gen(paths):
 def dict_gen(files):
     for f in files:
         try:
-            yield { "name": f.name.strip('.'), "json": json.load(f) }
+            print(f"Loading file {f.name}...")
+            yield json.load(f)
         except json.JSONDecodeError:
             print(f"{f.name} is not a valid JSON file")
         finally:
@@ -30,9 +48,9 @@ def instr_gen(instructions):
             b = bytearray([instruction_table.get(ins["operation"])])
             if ins["operation"] != "exe":
                 if ins["cycles"] is None:
-                    b.append([255])
+                    b.append(255)
                 else:
-                    b.append(max(ins["cycles"], 254))
+                    b.append(min(ins["cycles"], 254))
             yield b
         except KeyError:
             print(f"This instruction is not valid: {ins}")
@@ -41,37 +59,39 @@ def ito2bytes(x):
     if x < 256:
         return bytearray([x])
     else:
-        return bytearray([max(x/256, 255), x & 0xFF])
+        return bytearray([min(x/256, 255), x & 0xFF])
 
 def bytes_gen(dicts):
     for d in dicts:
         try:
+            print(f"Generating bytecode for {d['name']}...")
             b = bytearray([0x70, 0x66])  # sanity check: "pf" as first two bytes
-            b.append(d["name"].encode("utf-8"))
-            b.append(ito2bytes(d["memory"]))
-            b.append(max(len(d["instructions"], 255)))
+            b.extend(d["name"].encode("utf-8"))
+            b.append(0)
+            b.extend(ito2bytes(d["memory"]))
+            b.append(min(len(d["instructions"]), 255))
             instr_bytes = instr_gen(d["instructions"][:255])
             for ib in instr_bytes:
-                b.append(ib)
+                b.extend(ib)
 
             filename = "bytecode\\" + d["name"] + ".pf"
+            print(f"Writing {filename}...")
             with open(filename, "wb") as f:
                 f.write(b)
 
             yield filename
         except KeyError:
             print("JSON file is formatted incorrectly, skipping this file")
-        except: # this needs to be for the file io is fucked
-            pass 
-
-# todo: 
-#   check the command line for arguments
-#   if args: parse just those files
-#   else: scan directory for all .json files
-#
-#   initalize generators
-#   the loop should be over bytes_gen
-#   all it needs to do is print out each
-#   successful filename as it finishes
             
-        
+print("File conversion starting!")
+if len(sys.argv) < 2:
+    wg = walk_gen()
+else:
+    wg = (name for name in sys.argv[1:])
+
+pg = (name for name in wg if len(name.split('.')) > 1 and name.split('.')[-1] == 'json')
+fg = file_gen(pg)
+dg = dict_gen(fg)
+bg = bytes_gen(dg)
+for filename in bg:
+    print(f"{filename} has been successfully written!\n") 
